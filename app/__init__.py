03433dbb6eb0bfcfd22865163d2dbf4f8cc94b76
pedrobuzolin <buzolinpedro@gmail.com>
2026-03-10 15:12:23 -0300

    prepare project for production deploy

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,7 @@ def create_app(testing=False):
     app = Flask(__name__)
     app.config.from_object('config.Config')
 
-    try:
-        init_db(app.config['MONGO_URI'], testing)
-    except Exception as e:
-        print('Erro ao realizar a conexao com o banco de dados.')
+    init_db(app.config['MONGO_URI'], testing)
 
     from .routes.main import main_bp
     from .routes.user_routes import user_bp
